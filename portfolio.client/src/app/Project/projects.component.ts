4c48dd66c0f8c539f2e2a341c24810b49ea16ece
David Pérez Cruz <dperezc01@gmail.com>
2023-12-12 12:25:16 +0200

    fixed dark mode

--- portfolio.client/src/app/Project/projects.component.ts
+++ portfolio.client/src/app/Project/projects.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 import { Project } from '../Project';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'projects-root',
@@ -9,11 +10,16 @@ import { Project } from '../Project';
 })
 export class ProjectComponent implements OnInit {
   public projects: Project[] = [];
+  navbarOpen = false;
+  darkMode = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document, private http: HttpClient) { }
+
+  //constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.getProjects();
+    this.readfromcookie();
   }
 
   getProjects() {
@@ -26,6 +32,19 @@ export class ProjectComponent implements OnInit {
       }
     );
   }
-
+  toggleDarkMode() {
+    this.darkMode = !this.darkMode;
+    document.cookie = "darkMode=" + this.darkMode;
+    if (this.darkMode) {
+      this.renderer.addClass(this.document.body, 'bg-dark');
+      this.renderer.addClass(this.document.body, 'text-white');
+    } else {
+      this.renderer.removeClass(this.document.body, 'bg-dark');
+      this.renderer.removeClass(this.document.body, 'text-white');
+    }
+  }
+  readfromcookie() {
+    this.darkMode = document.cookie.includes("darkMode=true");
+  }
   title = 'Projects';
 }
--- portfolio.client/src/app/app.component.ts
+++ portfolio.client/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +10,30 @@ export class AppComponent {
   navbarOpen = false;
   darkMode = false;
 
+  constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) { }
+
   toggleNavbar() {
     this.navbarOpen = !this.navbarOpen;
   }
 
   toggleDarkMode() {
     this.darkMode = !this.darkMode;
+    document.cookie = "darkMode=" + this.darkMode;
+    this.updateBodyClass();
   }
+  readfromcookie() {
+    this.darkMode = document.cookie.includes("darkMode=true");
+    this.updateBodyClass();
+  }
+  ngOnInit() {
+    this.readfromcookie();
+  }
+  updateBodyClass() {
+    if (this.darkMode) {
+      this.renderer.setAttribute(this.document.body, 'data-bs-theme', 'dark');
+    } else {
+      this.renderer.setAttribute(this.document.body, 'data-bs-theme', 'light');
+    }
+  }
+
 }
