e1c5e847d31bffcb1bee55fc07293df75fec2ba9
David Pérez Cruz <dperezc01@gmail.com>
2024-05-21 10:52:13 +0200

    Added about page

--- portfolio.client/src/app/app.module.ts
+++ portfolio.client/src/app/app.module.ts
@@ -4,33 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MarkdownModule } from 'ngx-markdown';
 
 import { ProjectComponent } from './Project/projects.component';
-import { HomeComponent } from './Home/home.component';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SchoolWebappComponent } from './school-webapp/school-webapp.component';
 import { OnceSimulationComponent } from './once-simulation/once-simulation.component';
 import { StockSimulatorComponent } from './stock-simulator/stock-simulator.component';
 import { SpanishWordleSolverComponent } from './spanish-wordle-solver/spanish-wordle-solver.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'project', component: ProjectComponent },
+  { path: '', redirectTo: '/projects', pathMatch: 'full' },
+  { path: 'projects', component: ProjectComponent },
   { path: 'projects/SchoolWebapp', component: SchoolWebappComponent },
   { path: 'projects/OnceSimulation', component: OnceSimulationComponent },
   { path: 'projects/StocksSimulator', component: StockSimulatorComponent },
   { path: 'projects/SpanishWordleSolver', component: SpanishWordleSolverComponent },
+  { path: 'about', component: AboutComponent },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     ProjectComponent,
-    HomeComponent,
     SchoolWebappComponent,
     OnceSimulationComponent,
     StockSimulatorComponent,
-    SpanishWordleSolverComponent
+    SpanishWordleSolverComponent,
+    AboutComponent
   ],
   imports: [
     MarkdownModule.forRoot({
